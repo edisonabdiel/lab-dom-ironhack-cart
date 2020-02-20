3387b623c19f1439f735ddac3c236777854d6d6c
Edison Abdiel <edison.abdiel@soundcloud.com>
2020-02-20 21:28:02 +0100

    BER - WDPT EDISON DONE

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,37 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let cartElement = document.querySelector('#cart tbody');
+let calcButton = document.getElementById('calc');
+let deleteButton = document.querySelector('.btn-delete');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+
+ let calcAllHandler = () => {
+
+  let allProductElements = document.getElementsByClassName('product')
+
+  let price = allProductElements[0].querySelector('.pu span').innerText;
+  let qty = allProductElements[0].querySelector('input').value;
+  allProductElements[0].querySelector('.subtot span').innerText = Number(price) * Number(qty);
+
+  let price1 = allProductElements[1].querySelector('.pu span').innerText;
+  let qty1 = allProductElements[1].querySelector('input').value;
+  allProductElements[1].querySelector('.subtot span').innerText = Number(price1) * Number(qty1);
+  
+  let total = document.querySelector('h2 span').innerText = (Number(price) * Number(qty)) + (Number(price1) * Number(qty1));
+} 
+
+document.getElementsByClassName("qtyInput")[0].onchange = calcAllHandler;
+document.getElementsByClassName("qtyInput")[1].onchange = calcAllHandler;
+
+calcButton.onclick = calcAllHandler;
+
+let deleteProducts = () => {
+  document.getElementById('.bubble').removeChild('allProductElements')
 }
 
-function calcAll() {
-  // Iteration 1.2
-}
-$calc.onclick = calcAll;
+deleteButton.onClick = deleteProducts
+
+
+
+// .cloneNode()
+
+
+// querySelect
